0119d4ae5b0392410d67bcadaec175f6d5e68864
TommyB72 <45879898+TommyB72@users.noreply.github.com>
2026-04-17 13:50:45 -0500

    adjusted delete box, added reset box

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	mass = 0.16
 
 func _physics_process(delta: float) -> void:
-	#linear_velocity.x = 0.1
+	linear_velocity.x = 0.1
 	return
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 func GetBallCount() -> void:
 	if balls.get_child_count() > 0: return
 	get_tree().call_deferred("change_scene_to_file","res://Game.tscn")
+
+func _on_body_entered(body: Node3D) -> void:
+	body.get_parent().queue_free()
+
+func BallEnteredResetPlane(body: Node3D) -> void:
+	print(body)
