52efa5c6f5fddf8dc4fed094cdb2882a5d884b06
TommyB72 <45879898+TommyB72@users.noreply.github.com>
2026-04-17 13:22:28 -0500

    pool cue hit

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody3D
 
+func _ready() -> void:
+	mass = 0.16
+
 func _physics_process(delta: float) -> void:
-	linear_velocity.x = 0.1
+	#linear_velocity.x = 0.1
 	return
--- Scripts/pool_stick.gd
+++ Scripts/pool_stick.gd
@@ -1,28 +1,48 @@
-extends Node3D
+extends RigidBody3D
 
-var speed: float = 1.0
-var rotationSpeed: float = 1.0
-@export var rigid: RigidBody3D
+var speed: float = 0.4
+var rotationSpeed: float = 0.5
+var hitSpeed = 10
+@onready var parent = get_parent()
 
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_SPACE):
-		rigid.set_collision_mask_value(3, false)
-		return
-	rigid.set_collision_mask_value(3, false)
+func _ready() -> void:
+	set_collision_layer_value(3,false)
+	set_collision_mask_value(3,false)
+	gravity_scale = 0
+
+func _physics_process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_W):
-		position.x += speed * delta * sin(rotation.y)
-		position.z += speed * delta * cos(rotation.y)
+		parent.position.x += speed * delta * sin(parent.rotation.y)
+		parent.position.z += speed * delta * cos(parent.rotation.y)
 	if Input.is_key_pressed(KEY_S):
-		position.x -= speed * delta * sin(rotation.y)
-		position.z -= speed * delta * cos(rotation.y)
+		parent.position.x -= speed * delta * sin(parent.rotation.y)
+		parent.position.z -= speed * delta * cos(parent.rotation.y)
 	if Input.is_key_pressed(KEY_A):
-		position.x += speed * delta * cos(rotation.y)
-		position.z -= speed * delta * sin(rotation.y)
+		parent.position.x += speed * delta * cos(parent.rotation.y)
+		parent.position.z -= speed * delta * sin(parent.rotation.y)
 	if Input.is_key_pressed(KEY_D):
-		position.x -= speed * delta * cos(rotation.y)
-		position.z += speed * delta * sin(rotation.y)
-	if Input.is_key_pressed(KEY_LEFT): rotation.y += speed * delta
-	if Input.is_key_pressed(KEY_RIGHT): rotation.y -= speed * delta
+		parent.position.x -= speed * delta * cos(parent.rotation.y)
+		parent.position.z += speed * delta * sin(parent.rotation.y)
+	if Input.is_key_pressed(KEY_LEFT): parent.rotation.y += speed * delta
+	if Input.is_key_pressed(KEY_RIGHT): parent.rotation.y -= speed * delta
+	if Input.is_key_pressed(KEY_UP):
+		rotation.x += speed * delta
+		if rotation.x > 45*PI/180: rotation.x = 45*PI/180
+	if Input.is_key_pressed(KEY_DOWN):
+		rotation.x -= speed * delta
+		if rotation.x < 15*PI/180: rotation.x = 15*PI/180
+	move_and_collide(linear_velocity*delta)
 	
-	#if Input.is_key_pressed(KEY_UP): rotation.x += speed * delta
-	#if Input.is_key_pressed(KEY_DOWN): rotation.x -= speed * delta
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("hit"):
+		set_collision_layer_value(3,true)
+		set_collision_mask_value(3,true)
+		linear_velocity = Vector3(sin(parent.rotation.y)*hitSpeed,0,cos(parent.rotation.y)*hitSpeed)
+		#linear_velocity = Vector3(0,0,1)
+		
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	linear_velocity = Vector3.ZERO
+	position = Vector3.ZERO
+	set_collision_layer_value(3,false)
+	set_collision_mask_value(3,false)
+	
